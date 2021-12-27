e80448d852ebe947f67df92d45b170040c997827
asairyotaaa <91737985+asairyotaaa@users.noreply.github.com>
2021-12-27 16:59:25 +0900

    gggg

---
+++ src/App.js
@@ -0,0 +1,91 @@
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">有名な12のジブリ作品</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Image(props) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+        <img src={props.src} alt="cute dog!" />
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+function Gallery(props) {
+  const { urls } = props;
+  return (
+    <div className="columns is-vcentered is-multiline">
+      {urls.map((url) => {
+        return (
+          <div key={url} className="column is-3">
+            <Image src={url} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Main() {
+  const urls = [
+    " https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/7nO5DUMnGUuXrA4r2h6ESOKQRrx.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/xjJU6rwzLX7Jk8HFQfVW6H5guMC.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/byKAndF6KQSDpGxp1mTr23jPbYp.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHWmNr7m544fJ8eItsfNk8fs2Ed.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/avPMO5cnaGHgLaNiAIhy33WoQLm.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/mikKSEdk5kLhflWXbp4S5mmHsDo.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/oc2OB2KDmSRDMelKEAA1n4YRQL0.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rRLYX4RZIyloHSJwvZKAhphAjiB.jpg",
+  ];
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+        <Gallery urls={urls} />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Studio Ghibli images are retrieved from Studio Ghibli API</p>
+        <p>
+          <a href="https://ghibliapi.herokuapp.com/">Studio Ghibli API</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+function App() {
+    return (
+      <div>
+        <Header />
+        
+        <Main />
+        
+        <Footer />
+      </div>
+    );
+}
+
+export default App;
